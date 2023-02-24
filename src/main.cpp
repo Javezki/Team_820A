14aeef42c3f0edac1442ae2ff1eba6c66e68a48d
BuildTools <unconfigured@null.spigotmc.org>
2023-02-24 17:12:50 -0500

    Competition Build V1.0.0

--- src/main.cpp
+++ src/main.cpp
@@ -38,13 +38,17 @@ motor_group Shooting = motor_group(ShootingMotorA, ShootingMotorB);
 
 motor Tail = motor(PORT5, ratio18_1, false);
 
-competition Competition;
-
 /*vex-vision-config:begin*/
 vision::signature VisionSensor__DISC = vision::signature (1, 2185, 3213, 2699,-5301, -5083, -5192,3.9, 0);
 vision VisionSensor = vision (PORT1, 50, VisionSensor__DISC);
 /*vex-vision-config:end*/
 line LineTrackerA = line(Brain.ThreeWirePort.A);
+motor Crossbow = motor(PORT8, ratio18_1, false);
+
+motor Roller = motor(PORT20, ratio18_1, false);
+
+motor Intake = motor(PORT10, ratio18_1, false);
+
 
 
 
@@ -71,20 +75,33 @@ double CM_TO_INCH = 2.54;
 double INCH_TO_CM = 0.393;
 
 // Position in map
-double xPosition = 0;
-double yPosition = 0;
+// double xPosition = 0;
+// double yPosition = 0;
 
 // Mathemtical Constants
 double PI = 3.14159265; 
 
 // Robot Constants
-double WHEEL_RADIUSCM = 5.08;
-double WHEEL_RADIUSIN = 5.08 * CM_TO_INCH;
+// double WHEEL_RADIUSCM = 5.08;
+// double WHEEL_RADIUSIN = 5.08 * CM_TO_INCH;
+int shootSpeed = 40;
+int intakeSpeed = 40;
 
 void threadAlive();
 void initControls();
 void initModes();
 
+/* THE ROBOT CONTROLS AND MAPPING
+* R1 - Shooting (You can hold it)
+* R2 - Roller Adjustment (You can hold it)
+* L1 - Intake (You can hold it)
+* L2 - Tail (You can hold it)
+* Y - Field Expansions (Hold for 1 second)
+* Up Arrow - Increase Shooting Speed
+* Down Arrow - Decrease Shooting Speed
+* Left Joy Stick - Drive forward or backward (You can hold)
+* Right Joy Stick - Turn the robot (You can hold)
+*/
 
 int main() {
   initModes();
@@ -209,13 +226,32 @@ class RobotControls {
 
   public:
     static void manualShoot() {
+      Shooting.setVelocity(shootSpeed, percent);
       while (Controller1.ButtonR1.pressing()) {
         /*
         * Need testing 
         */
-        Shooting.spin(forward);
+        ShootingMotorA.spin(forward);
+        ShootingMotorB.spin(reverse);
       };
       Shooting.stop();
+    }
+
+  public:
+    static void manualIntake() {
+      Intake.setVelocity(intakeSpeed, percent);
+      while(Controller1.ButtonL1.pressing()) {
+        Intake.spin(forward);
+      }
+      Intake.stop();
+    }
+
+  public:
+    static void manualRoller() {
+      while (Controller1.ButtonR2.pressing()) {
+        Roller.spin(forward);
+      }
+      Roller.stop();
     }
 
   /**
@@ -235,6 +271,46 @@ class RobotControls {
       };
       Tail.stop();
     }
+
+      /* 
+  * Manual shooting of the crossbow, a wait function to 
+  * check if the button is still being held
+  */
+    
+  public:
+    static void manualCrossbow() {
+      wait(1, seconds);
+      if (Controller1.ButtonY.pressing()==false) return;
+      Crossbow.spinFor(forward, 90, degrees);
+      // printf("Y Button executed\n");
+    }
+
+  /*
+  * Adjust the speed of the shooting motors
+  */
+  public:
+  static void increaseShootingSpeed() {
+    Controller1.Screen.clearLine(3);
+    printf("The shooting motor speed: %d\n", shootSpeed);
+    if (shootSpeed == 100) {
+      Controller1.Screen.print("Shooting Speed 100");
+      return;
+    }
+    shootSpeed += 5;
+    Controller1.Screen.print("Shooting Speed %d", shootSpeed);
+  }
+
+  public:
+  static void decreaseShootingSpeed() {
+    Controller1.Screen.clearLine(3);
+    if (shootSpeed == 0) {
+      Controller1.Screen.print("Shooting Speed 0");
+      return;
+    }
+    shootSpeed -= 5;
+    Controller1.Screen.print("Shooting Speed %d", shootSpeed);
+  }
+
 };
 
 /** 
@@ -246,14 +322,18 @@ void initControls() {
   Controller1.Axis3.changed(controls.manualDirectionalMovement);
   Controller1.Axis1.changed(controls.manualRotationalMovement);
   Controller1.ButtonR1.pressed(controls.manualShoot);
-  Controller1.ButtonL1.pressed(controls.manualTail);
+  Controller1.ButtonR2.pressed(controls.manualRoller);
+  Controller1.ButtonL1.pressed(controls.manualIntake);
   Controller1.ButtonL2.pressed(controls.manualTail);
-}
+  Controller1.ButtonUp.pressed(controls.increaseShootingSpeed);
+  Controller1.ButtonDown.pressed(controls.decreaseShootingSpeed);
+  Controller1.ButtonY.pressed(controls.manualCrossbow);
+  }
 
 void initModes() {
   AutonomousPeriod ap;
+  competition Competition;
   Competition.autonomous(ap.onCompetition);
-  Competition.drivercontrol(threadAlive);
 }
 
 /** 
@@ -262,6 +342,6 @@ void initModes() {
 
 void threadAlive() {
   while (true) {
-    wait (0.5,seconds);
+    wait (1,seconds);
   }
 }
